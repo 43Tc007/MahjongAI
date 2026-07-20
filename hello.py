c835b2eb40a5a6aa3b2b5e8df1cc397cc47684e7
Charles0304 <ctc734.cc@gmail.com>
2026-07-20 18:14:46 +0800

    some experiments going on on test.ipynb

--- hello.py
+++
@@ -1,5 +0,0 @@
-from ray.rllib.algorithms.ppo import PPOConfig
-from ray.rllib.env.wrappers.pettingzoo_env import PettingZooEnv
-from ray.tune.registry import register_env
-from pettingzooenv import MahjongGameEnv
-
